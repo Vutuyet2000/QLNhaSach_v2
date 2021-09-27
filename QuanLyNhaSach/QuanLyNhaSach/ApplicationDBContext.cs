f586deadd09f86668bd09187286ceefb410776e3
hangnguyen1409 <1851010041Hang@ou.edu.vn>
2021-09-27 19:53:34 +0700

    QLNV-QLSach

---
+++ QuanLyNhaSach/QuanLyNhaSach/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using QuanLyNhaSach.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaSach
+{
+    class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext() : base("cnStr") { }
+
+        public DbSet<NhanVien> NhanVien { get; set;}
+        public DbSet<KhachHang> KhachHang { get; set; }
+        public DbSet<HoaDon> HoaDon { get; set; }
+        public DbSet<Sach> Sach { get; set; }
+    }
+}
